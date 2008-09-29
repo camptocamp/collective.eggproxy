f571f4da2473bfe67472111c3859ea39845f477d
b_mathieu <b_mathieu@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-09-29 19:19:05 +0000

    fix "package" test: paste fixture needs to be helped with relative urls

--- collective/eggproxy/tests/test_wsgi.py
+++ collective/eggproxy/tests/test_wsgi.py
@@ -13,11 +13,15 @@ def test_root_index():
 @with_setup(setup_func, teardown_func)
 def test_package_index():
     response = app.get('/collective.eggproxy')
-    assert 'collective.eggproxy' in response, response
+    assert '<title>collective.eggproxy</title>' in response, response
 
 @with_setup(setup_func, teardown_func)
 def test_package():
-    response = app.get('/collective.eggproxy')
-    response = response.click(index=0)
+    # trailing '/': without this, paste.fixture.TestResponse click results in:
+    # >>> urlparse.urljoin('/collective.eggproxy', 'collective.eggproxy-0.2.0.tar.gz'
+    # '/collective.eggproxy-0.2.0.tar.gz'
+    # actual url is: '/collective.eggproxy/collective.eggproxy-0.2.0.tar.gz'
+    response = app.get('/collective.eggproxy/')
+    response = response.click(index=0, verbose=True)
     assert ('Content-Encoding', 'gzip') in response.headers, response.headers
 
