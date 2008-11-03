7c5f9865fdc1c73c03dbd35d4f13fedaaaaeaf13
b_mathieu <b_mathieu@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-11-03 15:53:02 +0000

    override PackagesIndex.process_index: don't lowercase package names

--- collective/eggproxy/utils.py
+++ collective/eggproxy/utils.py
@@ -16,10 +16,21 @@
 ## Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 import os
 import tempfile
-import urllib
+import urllib2
+import urlparse
 import shutil
 from setuptools.package_index import PackageIndex as BasePackageIndex
-from setuptools.package_index import egg_info_for_url
+from setuptools.package_index import (
+    egg_info_for_url,
+    safe_name,
+    safe_version,
+    to_filename,
+    HREF,
+    htmldecode,
+    find_external_links,
+    PYPI_MD5,
+    )
+
 from pkg_resources import Requirement
 from collective.eggproxy.config import config
 
@@ -27,13 +38,55 @@ EGGS_DIR = config.get("default", "eggs_directory")
 INDEX_URL = config.get("default", "index")
 
 class PackageIndex(BasePackageIndex):
-    """This class overrides PackageIndex.can_add method to remove filter on
-    python major version: we want packages for all versions, all platforms
+    """
     """
     def can_add(self, dist):
-        """
+        """Overrides PackageIndex.can_add method to remove filter on python
+    major version: we want packages for all versions, all platforms
         """
         return True
+
+    def process_index(self,url,page):
+        """Process the contents of a PyPI page
+        Override: don't lowercase package name
+        """
+        def scan(link):
+            # Process a URL to see if it's for a package page
+            if link.startswith(self.index_url):
+                parts = map(
+                    urllib2.unquote, link[len(self.index_url):].split('/')
+                )
+                if len(parts)==2 and '#' not in parts[1]:
+                    # it's a package page, sanitize and index it
+                    pkg = safe_name(parts[0])
+                    ver = safe_version(parts[1])
+                    # changed "pkg.lower()" to "pkg"
+                    self.package_pages.setdefault(pkg,{})[link] = True
+                    return to_filename(pkg), to_filename(ver)
+            return None, None
+
+        # process an index page into the package-page index
+        for match in HREF.finditer(page):
+            scan( urlparse.urljoin(url, htmldecode(match.group(1))) )
+
+        pkg, ver = scan(url)   # ensure this page is in the page index
+        if pkg:
+            # process individual package page
+            for new_url in find_external_links(url, page):
+                # Process the found URL
+                base, frag = egg_info_for_url(new_url)
+                if base.endswith('.py') and not frag:
+                    if ver:
+                        new_url+='#egg=%s-%s' % (pkg,ver)
+                    else:
+                        self.need_version_info(url)
+                self.scan_url(new_url)
+
+            return PYPI_MD5.sub(
+                lambda m: '<a href="%s#md5=%s">%s</a>' % m.group(1,3,2), page
+            )
+        else:
+            return ""   # no sense double-scanning non-package pages
 
 
 INDEX = PackageIndex(index_url=INDEX_URL)
@@ -53,20 +106,14 @@ class IndexProxy(object):
        file_path = os.path.join(eggs_dir, 'index.html')
        html = open(file_path, 'w')
 
-       # FIXME: BEBIERE, this code sucks. You get all package names in lower
-       # case
-       #self.index.scan_all()
-       #package_names = self.index.package_pages.keys()
-       #package_names.sort()
+       self.index.scan_all()
+       package_names = self.index.package_pages.keys()
+       package_names.sort()
 
-       #print >> html, "<html><head><title>Simple Index</title></head><body>"
-       #for pack_name in package_names:
-       #    print >> html, '<a href="%s/">%s</a><br/>' % (pack_name, pack_name)
-       #print >> html, '</body></html>'
-
-       # so for now i add a quick fix to get real package names
-       page = urllib.urlopen(self.index.index_url)
-       html.write(page.read())
+       print >> html, "<html><head><title>Simple Index</title></head><body>"
+       for pack_name in package_names:
+           print >> html, '<a href="%s/">%s</a><br/>' % (pack_name, pack_name)
+       print >> html, '</body></html>'
 
        html.close()
        del html
