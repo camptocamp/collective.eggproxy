e4b05c0396f4fe248000e6426814183e40c976f2
reinout <reinout@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-19 11:06:46 +0000

    Preparing release 0.5.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ This module contains the tool of collective.eggproxy
 import os
 from setuptools import setup, find_packages
 
-version = '0.6dev'
+version = '0.5.1'
 
 README = os.path.join(os.path.dirname(__file__),
                       'collective', 'eggproxy', 'README.txt')
